2202b772bc4c2e101cde48c4783c915e2fc10936
DenisGuiraudet <guiraudet@live.fr>
2019-11-14 09:08:36 +0100

    front - constants

---
+++ services/front/src/components/edit/ExigenceCategory.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="exigenceCategory">
+  </div>
+</template>
+
+<script>
+
+
+export default {
+  components: {
+  },
+
+  data() {
+    return {
+    };
+  },
+  methods: {
+  },
+};
+</script>
+
+<style scoped lang="scss">
+</style>
